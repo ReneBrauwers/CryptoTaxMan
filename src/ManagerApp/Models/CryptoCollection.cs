1d412e949b5c0a2f383bc5ddec7d6032d4fa165d
Rene Brauwers <rene@brauwers.com.au>
2022-10-29 19:03:54 +1100

    added cryptocollection (collection of buys, starting point to do tax calculations for the sells)

---
+++ src/ManagerApp/Models/CryptoCollection.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagerApp.Models
+{
+    public sealed class CryptoCollection
+    {
+        public DateOnly CreatedOn { get; set; }
+        public string Name { get; set; }
+        public double Available { get; set; }
+        public double BoughtAt { get; set; }
+        public string Currency { get; set; }
+
+
+    }
+}
---
+++ src/ManagerApp/Services/CryptoCollectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using ManagerApp.Models;
+
+namespace ManagerApp.Services
+{
+    public static class CryptoCollectionFactory
+    {
+        public static List<CryptoCollection> CreateCryptoCollection(List<CryptoTransactionRecord> records)
+        {
+            var result = new List<CryptoCollection>();
+            foreach (var buyRecords in records.Where(x => x.TransactionType == "buy").OrderBy(x => x.TransactionDate))
+            {
+                result.Add(new CryptoCollection()
+                {
+                    CreatedOn = DateOnly.FromDateTime(buyRecords.TransactionDate),
+                    Available = buyRecords?.Amount ?? 0d,
+                    Name = buyRecords?.AmountAssetType ?? Guid.NewGuid().ToString(),
+                    BoughtAt = buyRecords?.ExchangeRateValue ?? 0d,
+                    Currency = buyRecords?.ExchangeRateCurrency ?? "AUD"
+                });
+            }
+
+            return result;
+        }
+    }
+}
