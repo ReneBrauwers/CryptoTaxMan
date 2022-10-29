20ba27fba7ddbe09c0e974f15d2cd7303eac88a3
Rene Brauwers <rene@brauwers.com.au>
2022-10-29 23:47:28 +1100

    generates initial tax report; however still contains a few issues which need to be resolved.

---
+++ src/ManagerApp/Models/CryptoTaxRecords.cs
@@ -0,0 +1,15 @@
+﻿namespace ManagerApp.Models
+{
+    public sealed class CryptoTaxRecords
+    {
+        public DateOnly SellDate { get; set; }
+        public string Name { get; set; }
+        public double SellAmount { get; set; }
+        public double BuyPrice { get; set; }
+        public double SellPrice { get; set; }
+        public string Currency { get; set; }
+        public double CapitalGainAmount { get; set; }
+
+        public string Calculation { get; set; }
+    }
+}
